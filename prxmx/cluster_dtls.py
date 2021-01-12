3122bd96d7eccff88873605be8b1ac6b39218796
kosimovsky <git.kosimovsky@gmail.com>
2021-01-12 22:38:08 +0300

    script is counting cpu & ram now

--- prxmx/cluster_dtls.py
+++ prxmx/cluster_dtls.py
@@ -2,20 +2,26 @@
 from proxmoxer import ProxmoxAPI
 from hurry.filesize import size, iec
 from argparse import ArgumentParser
+from termcolor import colored
 import sys
 import configparser
+
 
 def parse_args():
 	parser = ArgumentParser()
 	parser.add_argument("-c", "--cluster", dest="cluster", help="Enter the CLUSTER name", metavar="CLUSTER")
-	parser.add_argument("--usage", dest="usage", help="this usage!")
 	args = parser.parse_args()
+	if not len(sys.argv) > 1:
+		parser.print_help(sys.stderr)
+		sys.exit(1)
 	return args
+
 
 def read_config():
 	config = configparser.ConfigParser()
 	config.read('.config.ini', encoding='UTF-8')
 	return config
+
 
 def get_api(cluster_name):
 	c = read_config()
@@ -25,29 +31,45 @@ def get_api(cluster_name):
 	proxmox = ProxmoxAPI(host=proxmox_host, user=proxmox_user, password=passw, verify_ssl=False)
 	return proxmox
 
+
 def get_all_nodes(api):
 	nodes = []
 	for node in api.cluster.resources.get(type='node'):
 		nodes.append(node['node'])
-
 	print("There are nodes in this cluster:")
 	for node in api.cluster.resources.get(type='node'):
-		print("\t{0} --- {1} CPU --- {2}".format(node['node'], size(node['maxcpu'], system=iec),
-		                                       size(node['maxmem'], system=iec)))
+		print("\t{0} --- {1} CPU --- {2}".format(colored(node['node'], 'green'), size(node['maxcpu'], system=iec),
+		                                         size(node['maxmem'], system=iec)))
 	return nodes
+
+
+def get_all_vms_cpu(api, nodes):
+	for node in nodes:
+		cpu = 0
+		for vm in api.cluster.resources.get(type='vm'):
+			if node == vm['node'] and vm['status'] == 'running':
+				cpu += vm['maxcpu']
+		print("All allocated CPU on node {0} is: {1}".format(colored(node, 'green'), colored(size(cpu, system=iec), 'red')))
+
+
+def get_all_vms_ram(api, nodes):
+	for node in nodes:
+		ram = 0
+		for vm in api.cluster.resources.get(type='vm'):
+			if node == vm['node'] and vm['status'] == 'running':
+				ram += vm['maxmem']
+		print("All allocated RAM on node {0} is: {1}".format(colored(node, 'green'), colored(size(ram, system=iec), 'red')))
+
 
 def main():
 	args = parse_args()
-	if not len(sys.argv) > 1:
-		print(args.usage)
-	else:
-		cluster_name = args.cluster
-		api = get_api(cluster_name)
-		nodes = get_all_nodes(api)
-		for n in nodes:
-			print(n)
-		# for vm in api.cluster.resources.get(type='vm'):
-		# 	print("{0} --- {1} CPU --- {2}" .format(vm['id'], size(vm['maxdisk']), size(vm['maxmem'], system=iec)))
+	api = get_api(args.cluster)
+	nodes = get_all_nodes(api)
+	print(colored('\n##############################################\n', 'yellow'))
+	get_all_vms_cpu(api, nodes)
+	print(colored('\n##############################################\n', 'yellow'))
+	get_all_vms_ram(api, nodes)
+
 
 if __name__ == '__main__':
 	main()
